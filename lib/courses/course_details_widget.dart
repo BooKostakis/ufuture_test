a4a5c8a6dbf6d95df06335ba63ccacb0885b8904
Konstantin Bulanov <bookostakis@gmail.com>
2024-09-28 20:07:08 +0300

    WIP: courseDetails

--- lib/courses/course_details_widget.dart
+++ lib/courses/course_details_widget.dart
@@ -1,25 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:ufuture_test/courses/courses_repository.dart';
 import 'package:ufuture_test/widgets/app_bar_flexible_space_widget.dart';
 import 'package:ufuture_test/widgets/app_bar_title_widget.dart';
 
-class CourseDetailsWidget extends StatefulWidget {
+class CourseDetailsWidget extends StatelessWidget {
   final int courseId;
 
   const CourseDetailsWidget({super.key, required this.courseId});
 
   @override
-  State<CourseDetailsWidget> createState() => _CourseDetailsWidgetState();
-}
-
-class _CourseDetailsWidgetState extends State<CourseDetailsWidget> {
-  @override
   Widget build(BuildContext context) {
+    final courseList = CoursesRepository().courseList;
+    final course = courseList.firstWhere((element) => element.id == courseId);
     return Scaffold(
         appBar: AppBar(
           flexibleSpace: const AppBarFlexibleSpaceWidget(),
           title: const AppBarTitleWidget(),
         ),
-        body:
-            Center(child: Text('Детали courseID сейчас: ${widget.courseId} ')));
+        body: Center(
+            child: Column(
+          children: [
+            Text('Детали courseID сейчас: $courseId '),
+            Text('название: ${course.courseName}')
+          ],
+        )));
   }
 }
